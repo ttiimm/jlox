cbdddcbeb1ed42c8a64a7ca6a369f401f818e14d
Tim Likarish Ellis <likarish@gmail.com>
2025-05-08 22:02:42 -0400

    [Lox ch4.7] Identifiers and keywords

--- src/net/ttiimm/lox/Scanner.java
+++ src/net/ttiimm/lox/Scanner.java
@@ -2,10 +2,31 @@ package net.ttiimm.lox;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static net.ttiimm.lox.TokenType.*;
 
 public class Scanner {
+
+    private static final Map<String, TokenType> keywords = Map.ofEntries(
+            Map.entry("and", AND),
+            Map.entry("class", CLASS),
+            Map.entry("else", ELSE),
+            Map.entry("false", FALSE),
+            Map.entry("for", FOR),
+            Map.entry("fun", FUN),
+            Map.entry("if", IF),
+            Map.entry("nil", NIL),
+            Map.entry("or", OR),
+            Map.entry("print", PRINT),
+            Map.entry("return", RETURN),
+            Map.entry("super", SUPER),
+            Map.entry("this", THIS),
+            Map.entry("true", TRUE),
+            Map.entry("var", VAR),
+            Map.entry("while", WHILE)
+    );
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
@@ -83,12 +104,27 @@ public class Scanner {
                 break;
             default:
                 if (isDigit(c)) {
-                 number();
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     Reporter.error(line, "Unexpected character.");
                 }
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) {
+            advance();
+        }
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) {
+            type = IDENTIFIER;
+        }
+        addToken(type);
     }
 
     private void number() {
@@ -129,10 +165,6 @@ public class Scanner {
         addToken(STRING, value);
     }
 
-    private boolean isDigit(char c) {
-        return c >= '0' && c <= '9';
-    }
-
     private char peek() {
         if (isAtEnd()) {
             return '\0';
@@ -145,6 +177,20 @@ public class Scanner {
             return '\0';
         }
         return source.charAt(current + 1);
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private boolean match(char expected) {
