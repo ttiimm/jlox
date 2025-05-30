cae7837afa323206c2152205ce89ec406b7ea8af
Tim Likarish Ellis <likarish@gmail.com>
2025-05-29 21:32:32 -0400

    [Expr ch5.3] Add visitor pattern to expr and generator code

--- src/bin/GenerateAst.java
+++ src/bin/GenerateAst.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -26,10 +24,12 @@ private void defineAst(String outputDir, String baseName, List<String> types) th
     var path = Paths.get(outputDir, STR."\{baseName}.java");
     try (var writer = new PrintWriter(path.toFile(), StandardCharsets.UTF_8)) {
         writer.println("package net.ttiimm.lox;");
-        writer.println("");
-        writer.println("import java.util.List;");
-        writer.println("");
+        writer.println();
+        writer.println("// import java.util.List;");
+        writer.println();
         writer.println(STR."abstract class \{baseName} {");
+
+        defineVisitor(writer, baseName, types);
 
         for (var type : types) {
             var className = type.split(":")[0].trim();
@@ -37,14 +37,30 @@ private void defineAst(String outputDir, String baseName, List<String> types) th
             defineType(writer, baseName, className, fields);
         }
 
+        // the base accept() method
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
-        writer.println("");
+        writer.println();
     }
 
 }
 
+private void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println();
+    writer.println("  interface Visitor<R> {");
+
+    for (var type : types) {
+        var typeName = type.split(":")[0].trim();
+        writer.println(STR."    R visit\{typeName}\{baseName}(\{typeName} \{baseName.toLowerCase()});");
+    }
+
+    writer.println("  }");
+}
+
 private void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
-    writer.println("");
+    writer.println();
     writer.println(STR."  static class \{className} extends \{baseName} {");
     // Fields
     var fields = fieldList.split(", ");
@@ -53,7 +69,7 @@ private void defineType(PrintWriter writer, String baseName, String className, S
     }
 
     // Constructor
-    writer.println("");
+    writer.println();
     writer.println(STR."    \{className} (\{fieldList}) {");
 
     // Field assignment
@@ -63,6 +79,14 @@ private void defineType(PrintWriter writer, String baseName, String className, S
     }
     // End constructor
     writer.println("    }");
-    writer.println("  }");
+
+    // Visitor pattern
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println(STR."      return visitor.visit\{className + baseName}(this);");
+    writer.println("    }");
+
     // End class
+    writer.println("  }");
 }
